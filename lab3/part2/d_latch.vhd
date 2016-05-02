efb93b6aebe68c864dae14af0ed6ad8987c669eb
Eddie <eddie.dhakal@gmail.com>
2016-05-02 20:45:19 +1000

    ignored the qnot for d latch as it only has 1 output

--- lab3/part2/d_latch.vhd
+++ lab3/part2/d_latch.vhd
@@ -4,20 +4,22 @@ use ieee.std_logic_1164.all;
 entity d_latch is
 	port(
 		clk, d : in std_logic;
-		q, qnot : out std_logic);
+		q : out std_logic);
 end d_latch;
 
 architecture arc of d_latch is
-signal r_g, s_g, qa, qb : std_logic ;
-attribute keep : boolean;
-attribute keep of r_g, s_g, qa, qb : signal is true;
 
-begin
-	s_g <= d nand clk;
-	r_g <= (not d) nand clk; -- r = s'
-	qa <= s_g nand qb;
-	qb <= r_g nand qa;
+	signal r_g, s_g, qa, qb : std_logic ;
 
-	q <= qa;
-	qnot <= qb;
+	attribute keep : boolean;
+	attribute keep of r_g, s_g, qa, qb : signal is true;
+
+	begin
+		s_g <= d nand clk;
+		r_g <= (not d) nand clk;
+		qa <= s_g nand qb;
+		qb <= r_g nand qa;
+
+		q <= qa;
+
 end arc;
--- lab3/part2/part2.vhd
+++ lab3/part2/part2.vhd
@@ -4,15 +4,21 @@ use ieee.std_logic_1164.all;
 entity part2_main is
 	port(
 		sw : in std_logic_vector(1 downto 0);
-		ledr : out std_logic_vector(1 downto 0));
+		ledr : out std_logic_vector(0 downto 0));
 end part2_main;
 
 architecture arc of part2_main is
+	signal e, d, q : std_logic;
+
 	component d_latch is
 		port ( clk, d : in std_logic;
-			q, qnot : out std_logic);
+			q : out std_logic);
 	end component;
 	
 	begin
-		p: d_latch port map(sw(1), sw(0), ledr(0), ledr(1));
+		p: d_latch port map(e, d, q);
+		
+		e <= sw(1);
+		d <= sw(0);
+		ledr(0) <= q;
 end arc;
